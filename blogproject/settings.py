ba19a3d6e1826aeb43afed7fe0f89278c1be66a2
Ras0105 <smt29nov@gmail.com>
2026-05-12 18:41:28 +0530

    UI Changes

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,9 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     'blogapp',
-   
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +83,3 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
